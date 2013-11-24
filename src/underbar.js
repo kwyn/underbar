42285ff4be3e4e101ca0e2d18797512aea74c107
HarleyKwyn <kwyn.meagher@gmail.com>
2013-11-24 10:25:32 -0800

    _.uniq completed

--- src/underbar.js
+++ src/underbar.js
@@ -85,6 +85,13 @@ var _ = { };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var unique = [];
+    _.each(array, function(value){
+      if(_.indexOf(unique, value) == -1){
+        unique.push(value);
+      }
+    });
+    return unique;
   };
 
 
